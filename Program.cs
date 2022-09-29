aea5c5c4fb9b53e8882fc5b27357233cc95b0531
Kaceba <RobertoNinno23@outlook.it>
2022-09-29 19:45:47 +0200

    showcase

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         int[] Range = {1 , 100};
         
-        var Multiple = new RandomMultiple(); //Basta cambiare il nome della classe dichiarata e si puo
+        var Multiple = new SimpleMultiple(); //Basta cambiare il nome della classe dichiarata e si puo
                                            //Usufruire di tutti e 3 i servizi  
 
         int exit = Multiple.FindMultiple(Range[0], Range[1]);
