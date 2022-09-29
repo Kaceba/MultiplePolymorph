158f1bb519ad0f77436a885afd21118307367ab9
Kaceba <RobertoNinno23@outlook.it>
2022-09-29 17:32:12 +0200

    version 1

--- Program.cs
+++ Program.cs
@@ -3,6 +3,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int Number = 1;
+        
+        for (int i = 0; i <= 100; i++)
+        {
+            if(Number %2 == 0)
+            {
+                Console.WriteLine($"{Number} is a multiple of 2");
+            }
+            if(Number %3 == 0)
+            {
+                Console.WriteLine($"{Number} is a multiple of 3");
+            }
+            if(Number %5 == 0)
+            {
+                Console.WriteLine($"{Number} is a multiple of 5");
+            }
+            if(Number %7 == 0)
+            {
+                Console.WriteLine($"{Number} is a multiple of 7");
+            }
+
+           Number++;
+        }
+
+        Console.WriteLine("End of execution.");
     }
 }
