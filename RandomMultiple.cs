87ea17d4788fb81209bc7f649bb78b2e5b46eb57
Kaceba <RobertoNinno23@outlook.it>
2022-09-29 19:16:15 +0200

    final

---
+++ RandomMultiple.cs
@@ -0,0 +1,48 @@
+namespace MultiplePolymorph;
+
+using System.Linq;
+public class RandomMultiple
+{
+    public int FindMultiple(int Start, int End)
+    {
+
+        List<int> Values = new List<int>();
+
+        if (Start >= End)
+        {
+            Console.WriteLine("Incorrect values for range");
+            return 1;
+        }
+
+        Random rand = new Random();
+
+        for(int i = 0; i < 100; i++)
+        {
+            Values.Add(rand.Next(1, 101));
+        }
+
+        Values.Sort();
+
+        foreach (var Number in Values)
+        {
+            if(Number %2 == 0)
+            {
+                Console.WriteLine($"{Number} is a multiple of 2");
+            }
+            if(Number %3 == 0)
+            {
+                Console.WriteLine($"{Number} is a multiple of 3");
+            }
+            if(Number %5 == 0)
+            {
+                Console.WriteLine($"{Number} is a multiple of 5");
+            }
+            if(Number %7 == 0)
+            {
+                Console.WriteLine($"{Number} is a multiple of 7");
+            }
+        }
+
+        return 0;
+    }
+}
