c1b761a829e060345cefd5402536a09131f7a510
Kaceba <RobertoNinno23@outlook.it>
2023-02-20 12:27:17 +0100

    login

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
         int[] Range = {1 , 100};
         
         var Multiple = new SimpleMultiple(); //Basta cambiare il nome della classe dichiarata e si puo
-                                           //Usufruire di tutti e 3 i serviz
+                                           //Usufruire di tutti e 3 i servizi
 
         int exit = Multiple.FindMultiple(Range[0], Range[1]);
 
